534e70737d59fe2d95e149ddf5aefa4dbda63f48
Antj1 <95621875+Antj1@users.noreply.github.com>
2022-03-12 15:38:00 +0000

    Task2 - Equally weighted portfolio complete

--- stat11_ica/task2.R
+++ stat11_ica/task2.R
@@ -1,7 +1,40 @@
-library("quantmod") # load the quantmod library
-library("tseries") # load the tseries library
+library("tseries")
+library("zoo")
 
-price = get.hist.quote(instrument = "^gspc", start = "2000-01-03",end = "2020-01-04", quote="AdjClose") # download the prices,from January 1, 2000 until today
-y=diff(log(price)) # convert the prices into returns
-plot(y) # plot the returns
-z = coredata(y)
+library(PerformanceAnalytics)
+
+#Log (continuously compounded) Returns for S&P500 and FTSE100
+priceSP = get.hist.quote(instrument="^gspc", start = "2010-03-09", end = "2020-12-15", quote="AdjClose")
+y_sp=diff(log(priceSP))
+plot(y_sp)
+y_sp=coredata(y_sp)
+
+priceFTSE = get.hist.quote(instrument="^ftse", start = "2010-03-09", end = "2020-12-15", quote="AdjClose")
+y_ftse=diff(log(priceFTSE))
+plot(y_ftse)
+y_ftse=coredata(y_ftse)
+
+#Stock prices, and indices correlation matrix
+priceGE = get.hist.quote(instrument="ge", start = "2010-03-09", end = "2020-12-15", quote="AdjClose")
+priceMSFT = get.hist.quote(instrument="msft", start = "2010-03-09", end = "2020-12-15", quote="AdjClose")
+
+p <- cbind(priceSP, priceFTSE, priceGE, priceMSFT)
+y_individual = diff(log(p))#Log returns of each asset 
+y_individual = na.omit(y_individual)
+cor(y_individual, use="pairwise.complete.obs")
+
+#Now constructing an equally weighted portfolio with log returns
+
+
+equal_weights <- vector(mode = "numeric", length = dim(y_individual)[1])
+
+dates = fortify.zoo(y_individual[,0])
+
+
+for(i in 1:dim(y_individual)[1]){
+  a = rowSums(y_individual[i])/4
+  equal_weights[i] <- a}
+
+plot(equalp)
+plot(dates,equal_weights,type = "l")
+
